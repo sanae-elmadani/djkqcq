07eaff83acf32192de196120fd1e38f46ebd56cb
sanae-elmadani <sanaeelmadani3@gmail.com>
2022-02-28 22:12:15 +0100

    footer

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './components/component-footer/Footer';
+function App() {
+return (
+  <>
+  
+  <Footer />
+ 
+  </>
+	
+ 
+);
+}
+
+export default App;
+
